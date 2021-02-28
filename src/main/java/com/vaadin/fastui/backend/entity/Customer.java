b28f019f10c7905bda8a7af6ad9e575c039f56d8
Marcin <marcinpokusinski@gmail.com>
2021-02-28 22:28:57 +0100

    change

--- src/main/java/com/vaadin/fastui/backend/entity/Customer.java
+++ src/main/java/com/vaadin/fastui/backend/entity/Customer.java
@@ -1,4 +1,28 @@
 package com.vaadin.fastui.backend.entity;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+@Data
+@NoArgsConstructor
 public class Customer {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
 }
